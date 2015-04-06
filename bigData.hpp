a227ae4106852d8819e06750a42c9670aa861e6a
Tony <cba840910@gmail.com>
2015-04-06 21:08:21 +0800

    Add thread support

--- bigData.hpp
+++ bigData.hpp
@@ -40,10 +40,8 @@ namespace Data {
         std::uint32_t userID;
         std::uint32_t adID;
         std::uint32_t queryID;
-        std::uint8_t position;
-        std::uint8_t dept;
-        Key(uint32_t _UserID, uint32_t _AdID, uint32_t _QueryID, uint32_t _Position, uint32_t _Dept):
-            UserID(_UserID),AdID(_AdID),Query(_QueryID),Position(_Position),Dept(_Dept){}
+        std::uint8_t  position;
+        std::uint8_t  dept;
         bool operator==(const Key& rhs) const {
             return rhs.userID == userID && rhs.adID == adID && rhs.queryID == queryID &&
                    rhs.position == position && rhs.dept == dept;
@@ -54,7 +52,7 @@ namespace Data {
 
     };
 
-    struct KeyHash1 {
+    struct KeyHash {
         std::size_t operator()(const Key& key) const {
             const std::size_t prime = 92821;
             std::size_t hash = 486187739;
@@ -67,34 +65,18 @@ namespace Data {
         }
     };
 
-    std::hash<std::uint32_t> hash_fn;
-    struct KeyHash2 {
-        std::size_t operator()(const Key& key) const {
-            std::size_t hash = 1610612741;
-            hash = hash_fn( hash ^ key.UserID)
-            hash = hash_fn( hash ^ key.AdID)
-            hash = hash_fn( hash ^ key.QueryID)
-            hash = hash_fn( hash ^ key.Position)
-            hash = hash_fn( hash ^ key.Dept)
-            return hash;
-        }
-    };
     
     struct Value {
         std::uint16_t click;
         std::uint16_t impression;
 
-        bool operator==(const Value& rhs) const {
-            return rhs.click == click && rhs.impression == impression ;
+        void update( const Value & User::Value rhs ){
+            click += rhs.click;
+            impression += rhs.impression;
         }
-        bool operator!=(const Value& rhs) const {
-            return !operator==(rhs);
-        }
-        
     };
 
-    using Entry = std::pair<Key,Value>;
-    using Map   = fcmm::Fcmm<Key, unique_ptr<Value>, KeyHash1, KeyHash2> ;
+    using Map   = std::unordered_map< Data::Key, Data::Value, KeyHash >; 
 }
 
 namespace Ad {
@@ -103,11 +85,39 @@ namespace Ad {
         uint32_t clickCount;
         uint32_t impressionCount;
         double rate;  // totalClicks / totalImpressions
+
+        ClickThrough( uint32_t _clickCount ,uint32_t _impressionCount ,double _rate ): 
+            clickCount(_clickCount), impressionCount(_impressionCount), rate(_rate){} 
     };
+    using Information = vector< unique_ptr<AdInfo> >;
+    using ClickThroughTable = std::map< uint32_t,unique_ptr<ClickThrough> >;
     struct Ad {
         uint32_t advertiserID; 
-        vector< unique_ptr<AdInfo> > information;
-        std::map< UserID,unique_ptr<ClickThrough> > clickThroughTable; // Use default hash function
+        Information information;
+        // UserID --> clickThrough
+        ClickThroughTable clickThroughTable;
+
+        void updateClickTable( uint32_t userID , const Value& value ){
+           auto record = clickThroughTable.find( userID );
+           if( record == clickThroughTable.end() ){
+               clickThroughTable.emplace( 
+                   userID,  new ClickThrough( value->click,value->impression,0.0 )
+               );
+           }
+           else{
+               ClickThrough &entry = clickThroughTable[ userID ];
+               entry.impressionCount += value.impression;
+               if( value.click )
+                   entry.clickCount += value.click;
+           }
+
+        }
+        void calculateClickThroughEach(){
+           for( ClickThroughTable::iterator entry = table.begin(); entry != table.end(); entry++ ) {
+                if( entry->second->clickCount )
+                    entry->second->rate = (double)entry->second->clickCount / entry->second->impressionCount;
+           }
+        }
     };
     struct AdInfo {
         uint64_t displayURL; 
@@ -115,23 +125,28 @@ namespace Ad {
         int titleID; 
         int descriptionID;
     }
-    using Map = fcmm::Fcmm<AdID, unique_ptr<Ad> >; // Use default hash function
+    // AdID --> Ad
+    using Map = std::unordered_map< uint32_t, unique_ptr<Ad::Ad> >; 
+
+    void calculateClickThroughRate( Map::iterator it, Map::iterator until ){
+        for( ; it != until ; ++it )
+            it->calculateClickThroughEach();
+    }
 }
 
 namespace User {
 
-    struct Query {
-        int id;
-        Ad* ad;
-    };
-    using Ads     = std::set< uint32_t >; // A set of AdIDs
-    using Queries = std::vector< unique_ptr<User::Query> >;
+    using Ads    = std::set< uint32_t >; // A set of AdIDs
+    using Clicks = std::vector< Key* >; // Ideally, there won't be duplicated elements
     struct User {
         Ads impressions;  // All ads on which the user has at least one impression
-        Queries clicks;   // All queris on which the user has at least one click
+        Clicks clicks;   // All queries on which the user has at least one click
     };
-    using Entry = std::pair< UserID,unique_ptr<User> >;
-    using Map   = fcmm::Fcmm< UserID, unique_ptr<User> >; // Using default key hash
+    
+    using Map   = std::unordered_map< uint32_t, unique_ptr<User::User> >; // UserID --> User
+
+    void buildClicks( Data::Map::const_iterator from, Data::Map::const_iterator until ){
+    }
 
 }
 
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 #include <vector>
 #include <iterator>
 #include <cstdio>
@@ -18,19 +19,19 @@ const string command_get       = "get";
 const string command_impressed = "impressed";
 const string command_profit    = "profit";
 
-Data::Map dataMap(MAX_NUM_ENTRIES); // Key --> Value 
-User::Map userMap(MAX_NUM_USERS);   // UserID --> User
-Ad::Map adMap(MAX_NUM_ADS);         // AdID --> Ad
+Data::Map dataMap; // Key --> Value 
+User::Map userMap; // UserID --> User
+Ad::Map   adMap;   // AdID --> Ad
 
 void get( Data::Key key ){
-   const Data::Value& v = dataMap.at( key ); 
+   const Data::Value& v = dataMap[ key ]; 
    printf("%d %d\n", v.click, v.impression );
 }
 // output all (AdID, QueryID) pairs that user u has made at least one click
 void clicked( uint32_t UserID ){
-    const Queries& queries = (userMap.at( UserID ))->clicks;
-    for( Queries::iterator q = queries.begin(); q != queries.end(); q++ ){
-        printf("%d %d\n", q->ad->id, q->id);
+    User::Clicks& clicks = userMap[ UserID ]->clicks;
+    for( User::Clicks::iterator c = clicks.begin(); c != clicks.end(); c++ ){
+        printf("%d %d\n", c->adID, c->userID);
     }
 }
 //
@@ -39,27 +40,87 @@ void clicked( uint32_t UserID ){
 // that both users u1 and u2 has at least one impression on
 //
 void impressed( uint32_t UserID_1 , uint32_t UserID_2 ){
-    const Ads& user1 = (userMap.at( UserID_1 ))->impressions;
-    const Ads& user2 = (userMap.at( UserID_2 ))->impressions;
+    User::Ads& user1 = userMap[ UserID_1 ]->impressions;
+    User::Ads& user2 = userMap[ UserID_2 ]->impressions;
 
-    Ads common;
+    User::Ads common;
     std::set_intersection(user1.begin(),user1.end(),user2.begin(),user2.end(),
                           std::inserter(common,common.begin()));
     for( Ads::iterator ad = common.begin(); ad != common.bend(); ad++ ){
         printf("%d\n",ad->id);
-        for( vector<AdInfo>::iterator info = (ad->information).begin(); info != (ad->information).end(); info++ ){
-            printf("%d %d %d %d %d\n",info->displayURL,
-                                      ad->advertiserID,
-                                      info->keywordID,
-                                      info->titleID,
-                                      info->descriptionID);
+        for( Information::iterator info = (ad->information).begin(); info != (ad->information).end(); info++ ){
+            printf("%llu %d %d %d %d\n",info->displayURL,
+                                          ad->advertiserID,
+                                        info->keywordID,
+                                        info->titleID,
+                                        info->descriptionID);
         }
     }
 }
 // output the sorted (UserID), line by line, whose click-through-rate 
 // (total click / total impression) on `AdID` is greater than or equal to `clickThroughRateLowerBound`.
-void profit( uint32_t AdID, double clickThroughRateLowerBound ){
+void profit( uint32_t AdID, double lowerBound ){
+   Ad::ClickThroughTable& table = adMap[ adID ]->clickThroughTable;
+   for( ClickThroughTable::iterator entry = table.begin(); entry != table.end(); entry++ ) {
+        if( entry->second->rate >= lowerBound )
+            printf("%d\n", entry->first);
+   }
+}
 
+void buildClickThroughTable(){
+
+    int numThreads = 2;
+    std::vector<std::thread> threads;
+
+    int segLength = dataMap.size() / numThreads;
+    Data::Map::const_iterator from = dataMap.cbegin()
+    Data::Map::const_iterator until = next(from,segLength);
+    for (int i = 0;;) {
+        std::thread thread(User::buildClicks, from, until);
+        threads.push_back(std::move(thread));
+
+        if( ++i == numThreads )
+            break;
+
+        from = until;
+        advance(until,segLength);
+    }
+
+    if( end != dataMap.end() ){
+        User::buildClicks( from, dataMap.end() );
+    }
+
+    for (std::thread& thread : threads) {
+        thread.join();
+    }
+
+}
+void calculateRate(){
+
+    int numThreads = 2;
+    std::vector<std::thread> threads;
+
+    int segLength = adMap.size() / numThreads;
+    Ad::Map::iterator from = adMap.cbegin()
+    Ad::Map::iterator until = next(from,segLength);
+    for (int i = 0;;) {
+        std::thread thread(Ad::calculateClickThroughRate, from, until);
+        threads.push_back(std::move(thread));
+
+        if( ++i == numThreads )
+            break;
+
+        from = until;
+        advance(until,segLength);
+    }
+
+    if( end != adMap.end() ){
+        Ad::calculateClickThroughRate( end , adMap.end() );
+    }
+
+    for (std::thread& thread : threads) {
+        thread.join();
+    }
 }
 
 template <class int_type>
@@ -89,58 +150,49 @@ void read_data(const char* fileName){
 
 
     for (int i = 0 ; i < DATA_FILE_NUM_OF_LINE ; i++) {
-       std::unique_ptr<Data::Key>   key(new Data::Key);
-       std::unique_ptr<Data::Value> key(new Data::Value);
-       std::unique_ptr<Ad::AdInfo>  key(new Ad::AdInfo);
-       std::unique_ptr<Ad::Ad>      key(new Ad::Ad);
-//Click Impress URL_ID Ad_ID AdverID Dept Pos QueryID KeyWordID TitleID DescriptID UserID
-       value->click          = strToInt(&p);
-       value->impression     = strToInt(&p);
+       std::unique_ptr<Ad::AdInfo> adInfo(new Ad::AdInfo());
+       std::unique_ptr<Ad::Ad>     ad(new Ad::Ad());
+       Data::Key   key;
+       Data::Value value;
+
+       value.click           = strToInt(&p);
+       value.impression      = strToInt(&p);
        adInfo->displayURL    = strToInt(&p);
-       key->adID             = strToInt(&p);
+       key.adID              = strToInt(&p);
        ad->advertiserID      = strToInt(&p);
-       key->dept             = strToInt(&p);
-       key->position         = strToInt(&p);
-       key->queryID          = strToInt(&p);
+       key.dept              = strToInt(&p);
+       key.position          = strToInt(&p);
+       key.queryID           = strToInt(&p);
        adInfo->keywordID     = strToInt(&p);
        adInfo->titleID       = strToInt(&p);
        adInfo->descriptionID = strToInt(&p);
-       key->userID           = strToInt(&p);
-       auto insertState = dataMap.insert( std::make_pair(std::move(Key),std::move(Value)) );
+       key.userID            = strToInt(&p);
+
+       auto insertedData = dataMap.emplace( key, value ) ;
        // The key is already in map 
-       if( !insertState.second ){
-           insertState.first->second->update(
+       if( !insertedData.second )
+           insertedData.first->second->update( value );
 
-       }
-
-       /* Add entry to adMap */
-       // Ad* adBody = adMap.get( key.adID )
-       // if( !adBody )
-           // adMap.insert( std::make_pair( key->adID, ad ) ).first;
-       Ad& adBody = adMap.insertAndGet( std::make_pair( key->adID, ad ) );
-
-       adBody->information.push_back( adInfo ); 
-       auto record = adBody->clickThroughTable.find( userID );
-       if( record == adBody->clickThroughTable.end() )
-           adBody->clickThroughTable.insert( 
-               std::make_pair( userID, { value.click,value->impression,0,0 } )
-           );
-       else{
-           if(value->click)
-               adBody->clickThroughTable[ userID ].clickCount += value->click;
-           adBody->clickThroughTable[ userID ].impressionCount += value->impression;
-       }
+       auto insertedAdContent = adMap.emplace( key.adID, std::move(ad) ).first->second;
+       insertedAdContent->information.push_back( std::move(adInfo) ); 
+       insertedAdContent->updateClickTable( key.userID, value );
 
        /* add entry to userMap */
-       User* userBody = userMap.get( key->userID );
-       if( !userBody )
-           userMap.insert( std::make_pair( key->userID , unique_ptr<User::User>(new User::User()) );
-       userBody->impressions.insert( key->adID );  // The user has at least one impression
+       auto insertedUser = userMap.emplace( key.userID, unique_ptr<User::User>(new User::User()) ).first->second;
+       insertedUser->impressions.insert( key.adID ); // The user has at least one impression
+       if( value.click ) // If there's at least one click
+           insertedUser->clicks.push_back( &(insertedData.first->first) );
+
     }
 }
 int main(int argc, char *argv[])
 {
+    dataMap.reserve( MAX_NUM_ENTRIES ); 
+    userMap.reserve( MAX_NUM_USERS );
+      adMap.reserve( MAX_NUM_ADS );
+
     read_data(argv[1]);
+
     string command;
     while( cin >> command && command != command_quit ){
         cout<<"********************\n";
